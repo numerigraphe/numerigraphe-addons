41f256a36697652e8fed9951f30b785c55748ccc
Numerigraphe - Lionel Sausin <ls@numerigraphe.fr>
2012-01-12 16:03:25 +0100

    [ADD] stock_partner_ref: Let users set the partner context in the stock by location wizard

---
+++ stock_partner_ref/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Partner references on Product lists',
+    'version': '1.0',
+    "category" : "Generic Modules/Inventory Control",
+    'description': """This modules adds an option to select a Partner whose references will be displayed along with the Product list in the wizard 'Stock by Location'.
+It will also add the Partner's reference on any Product list when a Partner is in the context.""",
+    'author': u'Numérigraphe SARL',
+    'depends': [
+        'product',
+        'stock',
+    ],
+    'init_xml': [
+    ],
+    'update_xml': [
+        'product.xml',
+        'wizard/stock_location_product_view.xml'
+    ],
+    'demo_xml': [
+    ],
+    'installable': True,
+    'active': False,
+}
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ stock_partner_ref/wizard/stock_location_product.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from osv import fields, osv
+
+class stock_location_product(osv.osv_memory):
+    _name = 'stock.location.product'
+    _inherit = 'stock.location.product'
+    _columns = {
+        'partner_id': fields.many2one('res.partner',
+                                      "Display Partner's references"),
+    }
+    
+    def action_open_window(self, cr, uid, ids, context=None):
+        """Add the selected partner to the context"""
+        # Call the standard method
+        result = super(stock_location_product, self).action_open_window(
+                                                cr, uid, ids, context=context)
+        # Make sure we have proper dictionaries
+        if result is None:
+            result = {}
+        if result.get('context') is None:
+            result['context'] = {}
+        # Get the partner from the wizard and add it to the context
+        location_product = self.read(cr, uid, ids, ['partner_id'])
+        if location_product:
+            # XXX in trunk , change to  ... = location_product[0]['partner_id'][0]
+            result['context']['partner_id'] = location_product[0]['partner_id']
+        
+        return result
+            
+stock_location_product()
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
