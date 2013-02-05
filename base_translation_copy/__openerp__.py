5c65ade4bcc91f637b737552e9fabaa7a4c316c4
Numerigraphe - Lionel Sausin <ls@numerigraphe.fr>
2013-02-05 16:23:31 +0100

    [IMP] Don't copy translations of system models to English. Delete useless translations after they are copied to English.

--- base_translation_copy/__openerp__.py
+++ base_translation_copy/__openerp__.py
@@ -20,7 +20,7 @@
 
 {
     'name' : 'Copy Translations to English',
-    'version' : '1.0',
+    'version' : '1.1',
     'author' : u'Numérigraphe',
     'category' : 'Translation',
     'depends' : [],
