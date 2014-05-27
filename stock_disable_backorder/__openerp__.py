7f647c7eb7bf510527584a52b78729c333f53aa4
Lionel Sausin <ls@numerigraphe.com>
2014-05-27 18:52:08 +0200

    [ADD] stock_disable_backorder: module to work completely without partial stock operations: exceptions are pushed back to super-workflows instead (Sales/Purchases)

---
+++ stock_disable_backorder/__openerp__.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module is copyright (C) 2014 Numérigraphe SARL. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Disable back-orders',
+    'version': '1.0',
+    'author': u'Numérigraphe',
+    'category': 'Warehouse',
+    'description': """
+Disable back-orders on logistic operations
+==========================================
+
+This module disables the creation of back-orders for Stock Moves, Receptions
+and Deliveries. Doing improves the feedback given to the persons who give the
+orders (usually sales people, purchases managers or manufacturing managers),
+by bringing logistic problems directly to their attention.
+
+Simpler interface for logistic workers
+--------------------------------------
+Instead of letting partial operations be recorded through various wizards and
+buttons, those are instead entered by only 3 buttons displayed consistently on
+every documents:
+ - cancel the move
+ - split the stock move in two
+ - confirm the move
+
+Partial operations
+------------------
+Operations which cannot be done, or can only be done partially, are entered by
+simply canceling the corresponding Stock Move.
+Usually this Stock Move will have been associated to a procurement order:
+canceling the Stock Move will then put the Procurement Order in a special state
+which OpenERP will propagate to the original document (Purchase Order, Sale
+Order...).
+
+Purchase workflow
+-----------------
+The workflows of Purchase Orders is changed to take into account that some
+reception lines may be cancelled while others are done in the same reception
+picking. In such cases, the Purchase Order will go to the state
+"Delivery exception" as if the whole reception had been cancelled.
+
+API and external use
+--------------------
+This module exposes a simple method to split Stock Moves that is through
+webservices.
+This module does NOT disable the standard API for partial moves: it may still
+be used through XML-RPC or custom modules.
+
+Credits
+-------
+Split icon ©Yusuke Kamiyamane - http://p.yusukekamiyamane.com/. Licensed under
+a Creative Commons Attribution 3.0 License.
+""",
+    # FIXME: average price is not updated anymore
+    # FIXME: receptions cannot be recreated from purchase orders in exception state
+    # TODO: split the dependency on "purchase" to a 2nd module (auto-install)
+    'depends': [
+        'stock',
+        'purchase',
+    ],
+    'data': [
+        'wizard/stock_split_simple_view.xml',
+        'stock_view.xml',
+        'stock_workflow.xml',
+        'purchase_workflow.xml',
+    ],
+    'license': 'AGPL-3',
+}
---
+++ stock_disable_backorder/stock.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    This module is copyright (C) 2014 Numérigraphe SARL. All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm
+
+
+class StockPicking(orm.Model):
+    _inherit = 'stock.picking'
+
+    def test_complete(self, cr, uid, ids):
+        """Tests whether all the moves are done."""
+        move_obj = self.pool['stock.move']
+        move_ids = move_obj.search(cr, uid, [('picking_id', 'in', ids)])
+        return all([move.state == 'done'
+                    for move in move_obj.browse(cr, uid, move_ids)])
+
+
+class StockMove(orm.Model):
+    _inherit = 'stock.move'
+
+    def split(self, cr, uid, ids, split_qty, split_uos_qty, default=None,
+              context=None):
+        """Split Moves in two and return the new Stock Move ids.
+
+        Splitting consists of making a new copy of the initial move and
+        adjusting the quantities.
+        The original move gets qty = initial qty - split_qty.
+        The copy gets qty = split_qty.
+
+        @param ids: List of ID of the original Stock Moves
+        @param split_qty: Quantity to dispatch to new Stock Moves
+        @param split_uos_qty: Quantity in UoS to dispatch to new Stock Moves
+        @param default: Map of default values for the new Stock Moves
+        @return: List of IDs of the new lines created
+        """
+        if default is None:
+            default = {}
+        if split_qty <= 0 or split_uos_qty <= 0:
+            return []
+        split_ids = []
+        original_moves = self.read(
+            cr, uid, ids, ['product_qty', 'product_uos_qty', 'state'])
+        for move in original_moves:
+            initial_qty = move['product_qty']
+            initial_uos_qty = move['product_uos_qty']
+            if initial_qty <= split_qty or initial_uos_qty <= split_uos_qty:
+                continue
+            # Adjust the quantity on the original move
+            self.write(
+                cr, uid, ids,
+                {
+                    'product_qty': initial_qty - split_qty,
+                    'product_uos_qty': initial_uos_qty - split_uos_qty,
+                })
+            # Prepare the default values for the new Moves
+            default.update({'product_qty': split_qty,
+                            'product_uos_qty': split_uos_qty,
+                            'state': move['state']})
+            # XXX: copy() breaks traceability: is that OK?
+            split_ids.append(
+                self.copy(cr, uid, move['id'], default, context=context))
+        return split_ids
